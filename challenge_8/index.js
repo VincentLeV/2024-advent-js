a8874242e89d4539fb519e130a7760444b9dde25
Vincent Le <vincentle.fh@gmail.com>
2024-12-08 13:52:46 +0200

    Finish challenge 8

---
+++ challenge_8/index.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} indices - The reno indices
+ * @param {number} length - The length of the race
+ * @returns {string} The reno race
+ */
+function drawRace(indices, length) {
+  let result = ""
+  indices.forEach((position, i) => {
+    const getLineBreak = (index) => index < indices.length - 1 ? "\n" : ""
+    const getTab = (index) => " ".repeat(indices.length - 1 - index)
+
+    if (position < 0) position = length + position
+    if (position === 0) {
+      return result += getTab(i) + "~".repeat(length) + " /" + (i + 1) + getLineBreak(i)
+    }
+    result += getTab(i)
+    result += "~".repeat(position) + "r" + "~".repeat(length - position - 1) + " /" + (i + 1)
+    result += getLineBreak(i)
+  })
+  return result
+}
+
+console.log(drawRace([0, 5, -3], 10))
+/*
+  ~~~~~~~~~~ /1
+ ~~~~~r~~~~ /2
+~~~~~~~r~~ /3
+*/
+
+// console.log(drawRace([2, -1, 0, 5], 8))
+/*
+   ~~r~~~~~ /1
+  ~~~~~~~r /2
+ ~~~~~~~~ /3
+~~~~~r~~ /4
+*/
