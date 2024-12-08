192fd6d965ef75a1f7ae6c0f5b988a1de386dac5
Vincent Le <vincentle.fh@gmail.com>
2024-12-08 14:01:31 +0200

    Improve challenge 8

--- challenge_8/index.js
+++ challenge_8/index.js
@@ -3,21 +3,35 @@
  * @param {number} length - The length of the race
  * @returns {string} The reno race
  */
-function drawRace(indices, length) {
-  let result = ""
-  indices.forEach((position, i) => {
-    const getLineBreak = (index) => index < indices.length - 1 ? "\n" : ""
-    const getTab = (index) => " ".repeat(indices.length - 1 - index)
+// function drawRace(indices, length) {
+//   let result = ""
+//   indices.forEach((position, i) => {
+//     const getLineBreak = (index) => index < indices.length - 1 ? "\n" : ""
+//     const getTab = (index) => " ".repeat(indices.length - 1 - index)
 
-    if (position < 0) position = length + position
-    if (position === 0) {
-      return result += getTab(i) + "~".repeat(length) + " /" + (i + 1) + getLineBreak(i)
-    }
-    result += getTab(i)
-    result += "~".repeat(position) + "r" + "~".repeat(length - position - 1) + " /" + (i + 1)
-    result += getLineBreak(i)
-  })
-  return result
+//     if (position < 0) position = length + position
+//     if (position === 0) {
+//       return result += getTab(i) + "~".repeat(length) + " /" + (i + 1) + getLineBreak(i)
+//     }
+//     result += getTab(i)
+//     result += "~".repeat(position) + "r" + "~".repeat(length - position - 1) + " /" + (i + 1)
+//     result += getLineBreak(i)
+//   })
+//   return result
+// }
+
+function drawRace(indices, length) {
+  let result = "";
+  indices.forEach((position, i) => {
+    if (position < 0) position = length + position;
+    result += " ".repeat(indices.length - 1 - i);
+    position > 0 
+    ? result += "~".repeat(position) + "r" + "~".repeat(length - position - 1)
+    : result += "~".repeat(length);
+    result += " /" + (i + 1);
+    if (i < indices.length - 1) result += "\n";
+  });
+  return result;
 }
 
 console.log(drawRace([0, 5, -3], 10))
